3e710b695fa91fa643bfff32521d3090bbfde7cd
Aditya Taday <adi.taday@gmail.com>
2018-08-09 16:08:45 -0400

    Setting up repo

---
+++ litecoin.js
@@ -0,0 +1,14 @@
+/**
+ * Lite-Coin sample test transaction.
+ *
+ * This node script creates 2 addresses and simulates
+ * a sample transaction.
+ *
+ * @link   URL
+ * @file   litecoin.js
+ * @author Aditya Taday
+ * @since  08/09/2018
+ */
+
+import { liteCore } from 'litecore-lib'
+
